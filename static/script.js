126a447978ddd5659e4d5b26cc1fabad8e49965a
Tanner <tanner.cline88@gmail.com>
2025-02-09 17:31:16 -0700

    Updated navbar interation and layout. Created css and js files.

---
+++ static/script.js
@@ -0,0 +1,11 @@
+// Active class JS referenced from https://www.youtube.com/watch?v=ThSaI0kuez8
+const navLinks = document.querySelectorAll('.nav-link');
+const windowPathName = window.location.pathname;
+
+navLinks.forEach(navLinks => {
+  const linkPathName = new URL(navLinks.href).pathname;
+
+  if ((windowPathName === linkPathName)) {
+    navLinks.classList.add('active');
+  }
+})
