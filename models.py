82c68605b2915c5e8fe2b620e0777fe6dcd4c075
OSpaulitz <paulitzj@oregonstate.edu>
2025-02-25 20:03:00 +0000

    new changes integrated to tanner's new front end

--- models.py
+++ models.py
@@ -60,6 +60,7 @@ class CompletedChallenge(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     challenge_id = db.Column(db.Integer, db.ForeignKey('challenge.id'), nullable=False)
     completed_at = db.Column(db.DateTime, default=datetime.utcnow)
+    fully_completed = db.Column(db.Boolean, default=False)  # New flag
 
 class Favorite(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
